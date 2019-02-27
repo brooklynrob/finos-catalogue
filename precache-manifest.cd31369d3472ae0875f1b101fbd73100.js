41f432d8b740908581d47d1382c0a74c6d5a8a0d
Rob Underwood <runderwood5@gmail.com>
2019-02-26 23:41:56 -0500

    Updates

---
+++ precache-manifest.cd31369d3472ae0875f1b101fbd73100.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "54a78dd3615091214918",
+    "url": "/finos-catalogue/static/js/runtime~main.54a78dd3.js"
+  },
+  {
+    "revision": "a9d0c7f770f4d010ad12",
+    "url": "/finos-catalogue/static/js/main.a9d0c7f7.chunk.js"
+  },
+  {
+    "revision": "76aa52f1d9b998d19279",
+    "url": "/finos-catalogue/static/js/1.76aa52f1.chunk.js"
+  },
+  {
+    "revision": "a9d0c7f770f4d010ad12",
+    "url": "/finos-catalogue/static/css/main.90c099bb.chunk.css"
+  },
+  {
+    "revision": "b7b4e0b815a38efdd138054c5001412f",
+    "url": "/finos-catalogue/index.html"
+  }
+];
---
+++ test_data.re
@@ -0,0 +1,8 @@
+/* TEST DATA */
+
+let test_json = {
+  Activities.program_name: "my programName",
+  Activities.program_short_name: "program short",
+  Activities.program_homepage: "programName",
+  Activities.activity_name: "activity name"
+}
